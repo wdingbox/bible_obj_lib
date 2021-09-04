568a8aadb6913f92aa0579b26fdde9ebdfbf126d
wdingsoft <wdingbox@gmail.com>
2021-09-04 14:44:32 -0400

    add NHEB version obj

---
+++ raw_src/NHEB/ndjs_converter/cvt2obj.nd.js
@@ -0,0 +1,123 @@
+
+
+const fs = require('fs');
+const path = require('path');
+
+
+
+var BSNP_STD_BookCode = {
+    "Gen": ['Genesis', 'genesis', '创世纪',],
+    "Exo": ['Exodus', 'exodus', '出埃及记',],
+    "Lev": ['Leviticus', 'leviticus', '利未记',],
+    "Num": ['Numbers', 'numbers', '民数记',],
+    "Deu": ['Deuteronomy', 'deuteronomy', '申命记',],
+    "Jos": ['Joshua', 'joshua', '约书亚记',],
+    "Jug": ['Judges', 'judges', '士师记',],
+    "Rut": ['Ruth', 'ruth', '路得记',],
+    "1Sa": ['1_Samuel', '1-samuel', '撒母耳记上',],
+    "2Sa": ['2_Samuel', '2-samuel', '撒母耳记下',],
+    "1Ki": ['1_Kings', '1-kings', '列王记上',],
+    "2Ki": ['2_Kings', '2-kings', '列王记下',],
+    "1Ch": ['1_Chronicles', '1-chronicles', '历代志上',],
+    "2Ch": ['2_Chronicles', '2-chronicles', '历代志下',],
+    "Ezr": ['Ezra', 'ezra', '以斯拉记',],
+    "Neh": ['Nehemiah', 'nehemiah', '尼希米记',],
+    "Est": ['Esther', 'esther', '以斯帖记',],
+    "Job": ['Job', 'job', '约伯记',],
+    "Psm": ['Psalm', 'psalm', '诗篇',],
+    "Pro": ['Proverbs', 'proverbs', '箴言',],
+    "Ecc": ['Ecclesiastes', 'ecclesiastes', '传道书',],
+    "Son": ['SongOfSolomon', 'song-of-solomon', '雅歌',],
+    "Isa": ['Isaiah', 'isaiah', '以赛亚书',],
+    "Jer": ['Jeremiah', 'jeremiah', '耶利米书',],
+    "Lam": ['Lamentations', 'lamentations', '耶利米哀歌',],
+    "Eze": ['Ezekiel', 'ezekiel', '以西结书',],
+    "Dan": ['Daniel', 'daniel', '但以理书',],
+    "Hos": ['Hosea', 'hosea', '何西阿书',],
+    "Joe": ['Joel', 'joel', '约珥书',],
+    "Amo": ['Amos', 'amos', '阿摩司书',],
+    "Oba": ['Obadiah', 'obadiah', '俄巴底亚书',],
+    "Jon": ['Jonah', 'jonah', '约拿书',],
+    "Mic": ['Micah', 'micah', '弥迦书',],
+    "Nah": ['Nahum', 'nahum', '那鸿书',],
+    "Hab": ['Habakkuk', 'habakkuk', '哈巴谷书',],
+    "Zep": ['Zephaniah', 'zephaniah', '西番雅书',],
+    "Hag": ['Haggai', 'haggai', '哈该书',],
+    "Zec": ['Zechariah', 'zechariah', '撒迦利亚',],
+    "Mal": ['Malachi', 'malachi', '玛拉基书',],
+    "Mat": ['Matthew', 'matthew', '马太福音',],
+    "Mak": ['Mark', 'mark', '马可福音',],
+    "Luk": ['Luke', 'luke', '路加福音',],
+    "Jhn": ['John', 'john', '约翰福音',],
+    "Act": ['Acts', 'acts', '使徒行传',],
+    "Rom": ['Romans', 'romans', '罗马书',],
+    "1Co": ['1_Corinthians', '1-corinthians', '哥林多前书',],
+    "2Co": ['2_Corinthians', '2-corinthians', '哥林多后书',],
+    "Gal": ['Galatians', 'galatians', '加拉太书',],
+    "Eph": ['Ephesians', 'ephesians', '以弗所书',],
+    "Phl": ['Philippians', 'philippians', '腓立比书',],
+    "Col": ['Colossians', 'colossians', '歌罗西书',],
+    "1Ts": ['1_Thessalonians', '1-thessalonians', '帖撒罗尼迦前书',],
+    "2Ts": ['2_Thessalonians', '2-thessalonians', '帖撒罗尼迦后书',],
+    "1Ti": ['1_Timothy', '1-timothy', '提摩太前书',],
+    "2Ti": ['2_Timothy', '2-timothy', '提摩太后书',],
+    "Tit": ['Titus', 'titus', '提多书',],
+    "Phm": ['Philemon', 'philemon', '腓利门书',],
+    "Heb": ['Hebrews', 'hebrews', '希伯来书',],
+    "Jas": ['James', 'james', '雅各书',],
+    "1Pe": ['1_Peter', '1-peter', '彼得前书',],
+    "2Pe": ['2_Peter', '2-peter', '彼得后书',],
+    "1Jn": ['1_John', '1-john', '约翰一书',],
+    "2Jn": ['2_John', '2-john', '约翰二书',],
+    "3Jn": ['3_John', '3-john', '约翰三书',],
+    "Jud": ['Jude', 'jude', '犹大书',],
+    "Rev": ['Revelation', 'revelation', '启示录',],
+};
+var BSNP_STD_BookCodeAry = Object.keys(BSNP_STD_BookCode)
+
+
+//require("../../../jsdb/jsBibleStruct/All_Max_struct_json")
+var TmpMaxTxt = fs.readFileSync('../../../jsdb/jsBibleStruct/All_Max_struct_json.js', 'utf-8')
+var indx = TmpMaxTxt.indexOf("=")
+TmpMaxTxt = TmpMaxTxt.substr(indx + 1)
+var TmpObj = JSON.parse(TmpMaxTxt)
+
+
+fs.readFile('../NHEB_yhwh.txt', 'utf-8', (err, file) => {
+    const lines = file.split('\n')
+
+    var indx = 0, nheb_bkIDObj = {}
+    for (let line of lines) {
+        if (0 === line.indexOf("//")) continue
+        if (0 === line.length) continue
+        var mat = line.match(/^([a-zA-Z0-3]+)\s+([0-9]+)[\:]([0-9]+)\s+(.*)/)
+        if (mat) {
+            //console.log(mat)
+            //var sbkid = mat[0].trim()
+            var sbkid = mat[1].trim()
+            var schid = mat[2].trim()
+            var svsid = mat[3].trim()
+            var stext = mat[4].trim()
+
+
+            nheb_bkIDObj[sbkid] = 1
+            var kidxary = Object.keys(nheb_bkIDObj)
+            var stdbkid = BSNP_STD_BookCodeAry[kidxary.length - 1]
+
+            if (stdbkid !== sbkid) {
+                var schange = "raw:" + sbkid + " is changed to -----> bsnp: " + stdbkid
+                nheb_bkIDObj[sbkid] = schange
+            }
+            TmpObj[stdbkid][schid][svsid] = stext
+            indx++
+        }
+    }
+    console.log("total verses", indx)
+    console.log(nheb_bkIDObj)
+    console.log(BSNP_STD_BookCodeAry)
+
+    var str = JSON.stringify(TmpObj, null, 4)
+    str = "var NHEB_yhwh = \n" + str
+    fs.writeFileSync("NHEB_yhwh.json.js", str, "utf8")
+
+});
